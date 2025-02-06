ca113b7d8701be8452b3e7cb26545eabf4158cf0
anonimus007-700 <sahsafg80@gmail.com>
2025-02-06 13:02:57 +0200

    update

--- PitoRa.py
+++ PitoRa.py
@@ -3,19 +3,33 @@
 
 import socket
 
+from PIL import Image, ImageFile
+from io import BytesIO
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 sock = socket.socket()
 
 sock.bind(('127.0.0.1', 9090))
 
 sock.listen(1)
 
+print("Server is listening...")
+
 conn, addr = sock.accept()
 
+print(f"Connection established with {addr}")
+
 while True:
-    data = conn.recv(1024)
-    print(data)
+    data = conn.recv(4096)
+    if data:
+        img = Image.open(BytesIO(data))
+        #img = Image.open(BytesIO(data))
+        #img = data.decode('utf-8')
+        #img = eval(img)
+        img.show()
     if not data:
         break
-    conn.send(data.upper())
+    conn.send(b'success')
 
 
--- client.py
+++ client.py
@@ -7,32 +7,34 @@ import os
 
 from PIL import Image, ImageGrab
 from io import BytesIO
-from pyautogui import screenshot
 
-def image_to_data_url(image_path):
-    with Image.open(image_path) as img:
-        buffer = BytesIO()
-        img.save(buffer, format=img.format)
+def image_to_data_url(img):
+    img.save(byte_io, format='PNG')
 
-        img_bytes = buffer.getvalue()
+    img_bytes = buffer.getvalue()
 
-        img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
 
-        mime_type = f"image/{img.format.lower()}"
-        data_url = f"data:{mime_type};base64,{img_base64}"
+    mime_type = f"image/{img.format.lower()}"
+    data_url = f"data:{mime_type};base64,{img_base64}"
 
-        return data_url
+    return data_url
 
 sock = socket.socket()
+byte_io = BytesIO()
 
 sock.connect(('127.0.0.1', 9090))
 
 screen = ImageGrab.grab()
-data = image_to_data_url(screen)
+print(screen)
+# data = image_to_data_url(screen)
+screen.save(byte_io, 'JPEG')
+data = byte_io.getvalue()
 
-sock.send(data)
+sock.sendall(data)
 
-data = sock.recv(1024)
-sock.close()
-
-print(data)
+while True:
+    data = sock.recv(4096)
+    print(data)
+    if not data:
+        break
